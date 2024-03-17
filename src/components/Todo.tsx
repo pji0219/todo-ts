094aebcd8ec1d3549edb7071c1f1be9780fd9920
pji0219 <pji219@naver.com>
2024-03-18 03:23:19 +0900

    feat todo 구현

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,58 @@
+import { ChangeEvent, useContext, useState } from 'react';
+
+import { TodoContext } from '../context/TodoContext';
+import styles from './Todo.module.css';
+
 type Props = {
   id: any;
   text: string;
 };
 
 export default function Todo({ id, text }: Props) {
+  const [editedTodo, setEditedTodo] = useState(text);
+  const [editMode, setEditMode] = useState(false);
+  const { onUpdateTodoHandler, onRemoveTodoHandler } = useContext(TodoContext);
+
+  const onChangeTodoHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEditedTodo(event.target.value);
+  };
+
+  const onEditModeHandler = () => {
+    setEditMode((prev) => !prev);
+  };
+
   return (
-    <li>
-      <p>{id}</p>
-      <p>{text}</p>
+    <li className={styles.todo}>
+      {editMode ? (
+        <>
+          <input value={editedTodo} onChange={onChangeTodoHandler} />
+          <div>
+            <button
+              onClick={() => {
+                onUpdateTodoHandler(id, editedTodo);
+                onEditModeHandler();
+              }}
+            >
+              제출
+            </button>
+            <button onClick={onEditModeHandler}>취소</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <span>{text}</span>
+          <div>
+            <button onClick={onEditModeHandler}>수정</button>
+            <button
+              onClick={() => {
+                onRemoveTodoHandler(id);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        </>
+      )}
     </li>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,13 @@ import { useContext } from 'react';
 
 import Todo from '../components/Todo';
 import { TodoContext } from '../context/TodoContext';
+import styles from './Home.module.css';
 
 export default function Home() {
   const { todos } = useContext(TodoContext);
 
   return (
-    <ul>
+    <ul className={styles.todo_list}>
       {todos.map((todo) => (
         <Todo key={todo.id} id={todo.id} text={todo.text} />
       ))}
